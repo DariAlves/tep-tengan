f57a25c9a73ae630e691bd326dc273b3ec4e0979
Dari Alves <darieltondf@gmail.com>
2022-07-16 16:49:17 -0300

    :sunglasses: sequelize mysql

--- node-sequelize/src/database/connection.js
+++ node-sequelize/src/database/connection.js
@@ -2,21 +2,11 @@ import { Sequelize, DataTypes } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const database = {
-  db: process.env.DB_NAME,
-  db_user: process.env.DB_USERNAME,
-  db_password: process.env.DB_PASSWORD,
-  db_host: process.env.DB_HOST,
-};
+const { DB_NAME, DB_USERNAME, DB_PASSWORD, DB_HOST } = process.env;
 
-const sequelize = new Sequelize(
-  database.db,
-  database.db_user,
-  database.db_password,
-  {
-    host: database.db_host,
-    dialect: 'mysql',
-  }
-);
+const sequelize = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
+  DB_HOST,
+  dialect: 'mysql',
+});
 
 export { sequelize, DataTypes };
